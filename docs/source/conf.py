ad6852be81115e1f5133c944821138e1b526b01e
Dave Dittrich <dittrich@u.washington.edu>
2017-03-26 13:24:45 -0700

    Fix FQDN in DOCSURL

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -348,9 +348,9 @@ epub_exclude_files = ['search.html']
 #intersphinx_cache_limit = -1   # days to keep the cached inventories (0 == forever)
 #intersphinx_mapping = {
 #        'dimsocd': ("%s/dims/docs/dims-ocd" % os.environ['HOME'],
-#                    ('http://u12-dev-svr-1.prisem.washington.edu:8080/docs/develop/html/dims-ocd/objects.inv', None)),
+#                    ('http://app.devops.develop:8080/docs/develop/html/dims-ocd/objects.inv', None)),
 #        'dimsad': ("%s/dims/docs/dims-ad" % os.environ['HOME'],
-#                    ('http://u12-dev-svr-1.prisem.washington.edu:8080/docs/develop/html/dims-ad/objects.inv', None)),
+#                    ('http://app.devops.develop:8080/docs/develop/html/dims-ad/objects.inv', None)),
 #        'dimssr': ("%s/dims/docs/dims-sr" % os.environ['HOME'],
-#                    ('http://u12-dev-svr-1.prisem.washington.edu:8080/docs/develop/html/dims-sr/objects.inv', None))
+#                    ('http://app.devops.develop:8080/docs/develop/html/dims-sr/objects.inv', None))
 #}
